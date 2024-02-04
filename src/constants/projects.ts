2a0e89f85b557138113a8a2c0a66c58b83deb44d
Satyam Bajpai <bajpai1973satyam@gmail.com>
2024-02-04 21:10:54 +0000

    feat: create projects page

---
+++ src/constants/projects.ts
@@ -0,0 +1,11 @@
+import ReactIcon from "@/icons/projects/react-icon.astro";
+
+ const PROJECTS = [
+    {
+        title: 'Nuxt Playground',
+        subtitle: 'Some cool fact',
+        icon: ReactIcon 
+    }
+]
+
+export default PROJECTS;
