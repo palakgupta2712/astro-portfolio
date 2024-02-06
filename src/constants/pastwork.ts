90f9da2af85674d83e2364fad10314541f2e8409
iamitprakash <amitbcet2k15@gmail.com>
2024-02-07 01:52:19 +0530

    added past project

--- src/constants/pastwork.ts
+++ src/constants/pastwork.ts
@@ -3,27 +3,27 @@ import ReactIcon from "@/icons/projects/react-icon.astro";
 
 const PAST_PROJECTS = [
     {
-        title: 'Nuget Package past',
-        subtitle: 'Package to play with sting casing',
+        title: 'Nuget Package',
+        subtitle: 'Package to play with StRiNg cAsInG',
         link: 'https://www.nuget.org/packages/AlternateCase',
         icon: ReactIcon 
     },
     {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
+        title: 'Trabsmitting Int Data type in DB',
+        subtitle: 'Database Optimization',
+        link: 'https://github.com/iamitprakash/transmit-integers-efficiently-using-varint-encoding',
         icon: ReactIcon 
     },
     {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
+        title: 'Create Zip file in .Net',
+        subtitle: 'Create Zip file in .Net',
+        link: 'https://github.com/iamitprakash/create_zip_file_dot_Net',
         icon: ReactIcon 
     },
     {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
+        title: 'Coupab Code Impletation via HasSet',
+        subtitle: 'check whether a one time coupon has already been used. If so, a sui...',
+        link: 'https://github.com/iamitprakash/Hash_Set_Coupons',
         icon: ReactIcon 
     }
 ]
