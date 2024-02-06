e0338c06d41b5e38f2b16601dfba09faec969b4b
iamitprakash <amitbcet2k15@gmail.com>
2024-02-07 02:17:38 +0530

    Parking lot data added

--- src/constants/parking.ts
+++ src/constants/parking.ts
@@ -1,80 +1,16 @@
 import ReactIcon from "@/icons/projects/react-icon.astro";
 
- const PROJECTS  = [
-    {
-        title: 'Nuget Package',
-        subtitle: 'Package to play with StRiNg cAsInG',
-        link: 'https://www.nuget.org/packages/AlternateCase',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    }
-]
-const PROJECTS_CURRENTS  = [
-    {
-        title: 'Nuget Package',
-        subtitle: 'Package to play with sting casing',
-        link: 'https://www.nuget.org/packages/AlternateCase',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    }
-]
 const PARKING_LOT  = [
     {
-        title: 'Nuget Package1',
-        subtitle: 'Package to play with sting casing',
-        link: 'https://www.nuget.org/packages/AlternateCase',
+        title: 'Identity Service Provider',
+        subtitle: 'To Provide Identity service to all Application Inside Org Like Google Login and .....',
+        link: '',
         icon: ReactIcon 
     },
     {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
-        icon: ReactIcon 
-    },
-    {
-        title: 'Nuxt Playground',
-        subtitle: 'Some cool fact',
-        link: 'https://google.com',
+        title: 'Email Service/Server',
+        subtitle: 'Email Service/Server using GO, .Net and SQL',
+        link: '',
         icon: ReactIcon 
     }
 ]
