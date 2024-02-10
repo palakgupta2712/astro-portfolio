49bdf74bdc472b1e050beb7c7a671ccc95561bc1
yash raj <yashraj0207@outlook.com>
2024-02-10 20:01:47 +0530

    add react

---
+++ src/components/gallery.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react"
+
+export const Gallery = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-gray-50 space-x-4">
+      <h1>{count}</h1>
+      <button onClick={() => setCount(prev => prev + 1)}>add</button>
+      <button onClick={() => setCount(prev => prev - 1)}>sub</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </div>
+  )
+}
