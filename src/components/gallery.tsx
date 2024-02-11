5d3de0cd480b87e556245ce12a9138a1185bb3cc
yash raj <yashraj0207@outlook.com>
2024-02-11 21:55:24 +0530

    create carousel component and make it ineteractive

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,35 +1,94 @@
-import React, { useState, type ReactNode, useContext } from "react"
-import { create } from 'zustand'
+import React, { useEffect, useState, type ReactNode, useCallback } from "react";
+import { create } from 'zustand';
+
+import type { ImageMetadata } from "astro";
+const images = import.meta.glob<{ default: ImageMetadata }>(
+  "/src/assets/gallery/*.{jpeg,jpg,png}"
+);
 
 type TGalleryStoreState = {
   showCarousel: boolean
+  activeImageIndex: number
 }
 
 type TGalleryStoreActions = {
-  toggleCarousel: (showCarousel?: boolean) => void
+  setActiveImageIndex: (index: number) => void
+  toggleCarousel: (showCarousel?: boolean, index?: number) => void
 }
 
 type TGallyStore = TGalleryStoreState & TGalleryStoreActions
 
 const useGalleryStore = create<TGallyStore>()((set) => ({
   showCarousel: false,
-  toggleCarousel: (show) => set((state) => ({ showCarousel: show ?? !state.showCarousel })),
+  activeImageIndex: 0,
+  setActiveImageIndex: (index) => set({ activeImageIndex: index }),
+  toggleCarousel: (show, index) => set((state) => ({ showCarousel: show ?? !state.showCarousel, activeImageIndex: index ?? state.activeImageIndex })),
 }))
 
 type GalleryImageProps = {
   children: ReactNode
-  path: string
+  index: number
 }
 
-export const GalleryItem = ({ path, children }: GalleryImageProps) => {
+export const GalleryItem = ({ index, children }: GalleryImageProps) => {
   const { toggleCarousel } = useGalleryStore()
 
   return (
-    <div onClick={() => {
-      toggleCarousel()
-      console.log("hello", path)
-    }}>
+    <div onClick={() => toggleCarousel(true, index)}>
       {children}
+    </div>
+  )
+}
+
+const getResolvedImages = async () => {
+  const resolvedImages = await Promise.all(
+    Object.entries(images).map(async ([path, image]) => {
+      const { default: metadata } = await image()
+      return { path, metadata }
+    })
+  )
+
+  return resolvedImages
+}
+
+const Carousel = () => {
+  const { showCarousel, toggleCarousel, activeImageIdx, setActiveImageIdx } = useGalleryStore(store => ({ showCarousel: store.showCarousel, toggleCarousel: store.toggleCarousel, activeImageIdx: store.activeImageIndex, setActiveImageIdx: store.setActiveImageIndex }))
+
+  const [images, setImages] = useState<Awaited<ReturnType<typeof getResolvedImages>>>([])
+
+  const setResolvedImagesImages = useCallback(async () => {
+    const resolvedImages = await getResolvedImages()
+    setImages(resolvedImages)
+  }, [])
+
+  const handleEsc = useCallback((e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      toggleCarousel(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    setResolvedImagesImages()
+    document.addEventListener("keydown", handleEsc)
+    return () => document.removeEventListener("keydown", handleEsc)
+  }, [])
+
+  if (!showCarousel) return null
+
+  return (
+    <div className="fixed inset-0 w-full h-full">
+      <div className="w-full h-full z-[100] relative flex flex-col p-24">
+        <div className="absolute inset-0 w-full h-full bg-black/80 backdrop-blur-sm z-50" onClick={() => toggleCarousel(false)} />
+
+        <div className="max-w-7xl mx-auto flex-1 relative z-[100]">
+          <img src={images[activeImageIdx]?.metadata.src} className="w-max max-w-full h-auto aspect-video object-contain" />
+        </div>
+
+        <div className="relative z-[100] w-max mx-auto flex items-center text-white justify-center gap-4">
+          <button className="p-3" onClick={() => setActiveImageIdx(activeImageIdx <= 0 ? 0 : activeImageIdx - 1)}>prev</button>
+          <button className="p-3" onClick={() => setActiveImageIdx(activeImageIdx >= images.length - 1 ? activeImageIdx : activeImageIdx + 1)}>next</button>
+        </div>
+      </div>
     </div>
   )
 }
@@ -39,20 +98,13 @@ type GalleryProps = {
 }
 
 export const Gallery = ({ children }: GalleryProps) => {
-  const [count, setCount] = useState(0)
-  const { showCarousel } = useGalleryStore()
-  console.log(showCarousel)
-
   return (
-    <div className="text-gray-50 space-x-4">
-      <h1>{count}</h1>
-      <button onClick={() => setCount(prev => prev + 1)}>add</button>
-      <button onClick={() => setCount(prev => prev - 1)}>sub</button>
-      <button onClick={() => setCount(0)}>reset</button>
-
-      <div>
+    <div>
+      <div className="grid grid-cols-3 gap-4 py-8">
         {children}
       </div>
+
+      <Carousel />
     </div>
   )
 }
