900de4aa9dd011e55d6d37c0e1beff98a66c65f4
palakgupta2712 <palakgupta2712@gmail.com>
2024-02-16 00:25:45 +0530

    project-section-update: icons and styling updated

--- src/constants/pastwork.ts
+++ src/constants/pastwork.ts
@@ -1,4 +1,4 @@
-import ReactIcon from "@/icons/projects/react-icon.astro";
+import PastProjectsIcon from "@/icons/projects/past-projects-icon.astro";
 
 
 const PAST_PROJECTS = [
@@ -6,61 +6,61 @@ const PAST_PROJECTS = [
         title: 'AlternateCase Nuget Package',
         subtitle: 'Package to play with StRiNg cAsInG',
         link: 'https://www.nuget.org/packages/AlternateCase',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: 'InvertBinaryTree Nuget Package',
         subtitle: 'Package to Invert Binary Tree',
         link: 'https://www.nuget.org/packages/InvertBinaryTree',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     }, 
     {
         title: '[GO]Load Balancer',
         subtitle: 'Tried to create an Intelligent Load Balancer',
         link: 'https://github.com/iamitprakash/load-balancer-go',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: '[GO]Auth Server',
         subtitle: 'Tried to create an Authentication Server to plug & Play into any Microservice Architecture',
         link: 'https://github.com/iamitprakash/Auth-server-go',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: 'Transmitting Int Data type in DB',
         subtitle: 'Database Optimization',
         link: 'https://github.com/iamitprakash/transmit-integers-efficiently-using-varint-encoding',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: 'Create Zip file in .Net',
         subtitle: 'Create Zip file in .Net',
         link: 'https://github.com/iamitprakash/create_zip_file_dot_Net',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: 'Coupon Code implementation via HashSet',
         subtitle: 'check whether a one time coupon has already been used. If so, a sui...',
         link: 'https://github.com/iamitprakash/Hash_Set_Coupons',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: 'Sample API Server in GO',
         subtitle: 'Develop a Go server with two endpoints (/process-single and /process-concurrent)...',
         link: 'https://github.com/iamitprakash/Go-Server-for-Sorting-Arrays',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: 'Bloom Filter in .Net',
         subtitle: 'Created Bloom Filter in C#, .Net',
         link: 'https://github.com/iamitprakash/bloom_filter_in_dot_net',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     },
     {
         title: '[GO]Concurrent Thread safe Queue',
         subtitle: 'An experiment to make Queue Thread safe',
         link: 'https://github.com/iamitprakash/concurrent_thread_safe_queue',
-        icon: ReactIcon 
+        icon: PastProjectsIcon 
     }
 ]
 
